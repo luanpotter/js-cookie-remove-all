9cdf35d984fba5ddf549afc45deee3f328dc9953
Luan Nico <luanpotter28@hotmail.com>
2016-01-10 00:23:16 -0200

    folder structure

--- index.js
+++
@@ -1,7 +0,0 @@
-var Cookies = require('js-cookie');
-
-Cookies.removeAll = function (attributes) {
-	Object.keys(Cookies.get()).forEach(function (cookie) {
-		Cookies.remove(cookie, attributes);
-	});
-};
